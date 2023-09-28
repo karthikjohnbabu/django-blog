7b983b19b51823be8849b2c02d2b73cf1b0ce10e
Karthik Babu <karthikbabu@Karthiks-MacBook-Air.local>
2023-09-28 10:47:26 +0530

    category crud functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -31,5 +31,9 @@ urlpatterns = [
     path('login/',views.login, name='login'),
     path('logout/',views.logout, name='logout'),
 
+    # Dashboard
+    path('dashboard/', include('dashboards.urls')),
+    
+
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
